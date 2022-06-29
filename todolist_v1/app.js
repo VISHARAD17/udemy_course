ce51780b77bffcb6391bc80b76cd991f296e47d8
Visharad <visharadborsutkar777@gmail.com>
2022-06-29 14:05:47 +0530

    new ejs challege udemy course

--- todolist_v1/app.js
+++ todolist_v1/app.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const date = require(__dirname + '/date.js');
 // tells the app to use ejs as view engine
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true})); // instead of body parser
-
+app.use(express.static('public'));
 var items = [];
-
+var workItems = [];
 app.get("/", (req, res) => {
-    const today = new Date();
-    var options = {
-        weekday : 'long',
-        day : 'numeric',
-        month : 'long'
-    };
+    
+    let day = date.getDate();
 
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render('list', {kindOfDay: day, newListItem: items});
+    res.render('list', {listTitle: day, newListItem: items});
 });
 
+// new route for work page
+app.get("/work", (req, res) => {
+    res.render('list', {listTitle:"Work List", newListItem: workItems});
+});
+
+app.post("/work", (req, res) => {
+
+});
 
 app.post("/", (req, res) => {
+    if(req.body.res)
+    console.log(req.body)
     var item = req.body.newItem;
     // console.log(item);
     items.push(item);
---
+++ todolist_v1/date.js
@@ -0,0 +1,13 @@
+exports.getDate = function() {
+    const today = new Date();
+    let options = {
+        weekday : 'long',
+        day : 'numeric',
+        month : 'long'
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}
+
