069f7ee938e53ca63db2c10047f2921629b2c3ab
Visharad <visharadborsutkar777@gmail.com>
2022-06-29 23:26:15 +0530

    new module from udemy course angela yu

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -9,25 +9,52 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const posts = [];
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    content: homeStartingContent,
+    posts: posts
+  });
+  // console.log(posts);
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {content: aboutContent});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {content: contactContent});
+});
+
+app.get('/compose', (req, res) =>{
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) =>{
+  const post = {
+    title : req.body.postTitle,
+    content : req.body.postbody
+  };
+  posts.push(post); // posting all the posts in thte global variable
+  res.redirect('/');
+  // console.log(req.body.postbody);
+});
+
+app.get('/posts/:postName', (req, res) => {
+  const requestedTitle = req.params.postName;
+  // console.log(req.params.postName);
+  posts.forEach(function(post){
+    const storeTitle = post.title;
+    if(storeTitle == requestedTitle) console.log("Matched");
+  });
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3001, function() {
+  console.log("Server started on port 3001");
 });
